86854045bfe2c00e64cff063a6cfd1c01045b8c0
safinamoideen <122775542+moideensafina@users.noreply.github.com>
2025-04-19 12:48:01 +0530

    sixth commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '@pages/Home'
+import Home from '@pages/Home.jsx'
 import About from '@pages/About'
 import SignIn from '@pages/SignIn'
 import Dashboard from '@pages/Dashboard'
